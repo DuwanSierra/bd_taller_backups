58e297faa591676ae8c04170f05b4668714abffb
Duwan Sierra <desierrag@gmail.com>
2022-07-13 19:02:44 -0500

    feature: add init container from scripts with import data from csv

--- data/commands/comandos_ejecutar.sh
+++ data/commands/comandos_ejecutar.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-psql -u root -c 'create database library;'
-psql -U root -d library -a -f library_create.sql
-psql -U root -d library -c "\copy ubicacion FROM '/var/lib/postgresql/csv/Ubicacion.csv' DELIMITER ';' CSV HEADER;"
-psql -U root -d library -c "select * from ubicacion"
+echo "Creating DB";
+sleep 5;
+psql "postgresql://root:root@database/test_db" -c 'create database library;';
+echo "Creating tables";
+psql "postgresql://root:root@database/library" -a -f "/var/lib/postgresql/scripts/library_create.sql";
+sleep 5;
+echo "Creating inserts";
+psql "postgresql://root:root@database/library" -c "\copy ubicacion FROM '/var/lib/postgresql/csv/ubicacion.csv' DELIMITER ';' CSV HEADER;";
+psql "postgresql://root:root@database/library" -c "\copy autor FROM '/var/lib/postgresql/csv/autor.csv' DELIMITER ';' CSV HEADER;";
+psql "postgresql://root:root@database/library" -c "\copy usuario FROM '/var/lib/postgresql/csv/usuario.csv' DELIMITER ';' CSV HEADER;";
+psql "postgresql://root:root@database/library" -c "\copy editorial FROM '/var/lib/postgresql/csv/editorial.csv' DELIMITER ';' CSV HEADER;";
+psql "postgresql://root:root@database/library" -c "\copy telefono FROM '/var/lib/postgresql/csv/telefono.csv' DELIMITER ';' CSV HEADER;";
+psql "postgresql://root:root@database/library" -c "\copy libro FROM '/var/lib/postgresql/csv/libro.csv' DELIMITER ';' CSV HEADER;";
+psql "postgresql://root:root@database/library" -c "\copy ejemplar FROM '/var/lib/postgresql/csv/ejemplar.csv' DELIMITER ';' CSV HEADER;";
+psql "postgresql://root:root@database/library" -c "\copy escribe FROM '/var/lib/postgresql/csv/escribe.csv' DELIMITER ';' CSV HEADER;";
+psql "postgresql://root:root@database/library" -c "\copy prestamo FROM '/var/lib/postgresql/csv/prestamo.csv' DELIMITER ';' CSV HEADER;";
